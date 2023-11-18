eef98423fb5e30602b54c84200eb6dc38634ec08
Tehedor <101796030+Tehedor@users.noreply.github.com>
2023-11-18 19:39:52 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -29,3 +29,41 @@ mongoose.connect('mongodb://localhost:27017/test');
 
 // Create schema
 var commentsSchema = new mongoose.Schema({
+    name: String,
+    comment: String
+});
+
+// Create model
+var Comments = mongoose.model('Comments', commentsSchema);
+
+// Create web server
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Send all comments to client
+app.get('/comments', function(req, res){
+    Comments.find({}, function(err, comments){
+        res.send(comments);
+    });
+});
+
+// Receive new comment from client
+app.post('/comments', function(req, res){
+    var newComment = new Comments(req.body);
+    newComment.save(function(err){
+        if(err) throw err;
+        io.emit('newComment', req.body);
+        res.sendStatus(200);
+    });
+});
+
+// Create socket
+io.on('connection', function(socket){
+    console.log('A user connected');
+});
+
+// Start server
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
