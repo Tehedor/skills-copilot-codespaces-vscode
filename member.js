d5c04040379faefe5d13586779043f0b9c44709b
Tehedor <101796030+Tehedor@users.noreply.github.com>
2023-11-18 19:31:12 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/member/member.html',
+    controller: 'MemberController',
+    controllerAs: 'memberCtrl',
+    bindToController: true
+  };
+}
